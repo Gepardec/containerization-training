107423130768f2f33f744545cf9a4e8ff9bb85e7
Thomas Herzog <thomas.herzog@gepardec.com>
2022-07-15 13:34:30 +0200

    - Migrated the containerization training from Gepardec/presentations repository to this repository

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # containerization-training
-This repository holds the sources for the containerization training
+
+This repository holds the sources for the containerization training. 
+This training will learn trainees to understand the fundementals of Containerization via Docker.
+
+## Building
+
+This presentation can be rendered as HTMl and PDF on Unix Hosts via nodejs, revealjs, deckjs and Docker.
+
+`./make.sh` will render the HTMl and PDf slides.
+
+## Apple M1/2 (ARM64)
+
+The used decktape container image has no ARM plattform support for now, but works on ARM if you build it yourself. +
+Set the the default conatienr image architecture via an environment variable `export DOCKER_DEFAULT_PLATFORM=linux/arm64`. +
+Build the container image with `docker build --tag astefanutti/decktape:3.1.0 .` on your MAC and it will build the image for the `linux/arm64` architecture.
+
+# contributions
+- [Thomas Herzog](https://github.com/cchet)
---
+++ make.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+####################### 
+# READ ONLY VARIABLES #
+#######################
+
+readonly IMAGE_NAME=containerization-training-renderer
+readonly PRESENTATION_NAME=Containerization-Training
+
+#################### 
+# GLOBAL VARIABLES #
+####################
+
+########## 
+# SOURCE #
+##########
+set -ex
+
+rm -rf docs
+mkdir docs
+docker build -t $IMAGE_NAME .
+docker container rm -f $IMAGE_NAME >/dev/null
+docker run -d --name $IMAGE_NAME $IMAGE_NAME
+docker cp $IMAGE_NAME:/opt/revealjs/dist   docs/dist
+docker cp $IMAGE_NAME:/opt/revealjs/plugin docs/plugin
+docker cp $IMAGE_NAME:/opt/revealjs/src    docs/src
+
+chmod -R 777 docs
+
+docker container rm -f $IMAGE_NAME >/dev/null
+docker run -d -p 8080:8080 --entrypoint=/opt/revealjs/src/bin/present.py --name $IMAGE_NAME $IMAGE_NAME 8080 
+sleep 2
+docker cp $IMAGE_NAME:/opt/revealjs/index.html docs/$PRESENTATION_NAME.html
+docker run --rm --net=host -t -v $(pwd):/slides astefanutti/decktape:3.1.0 reveal --size=1920x1080 --pause 2000 --load-pause 2000 http://localhost:8080/ docs/$PRESENTATION_NAME.pdf
+docker container rm -f $IMAGE_NAME >/dev/null
