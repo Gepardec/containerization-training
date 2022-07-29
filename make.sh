1bbed542f9e144339c0246c806eaa0d5b82a0274
Thomas Herzog <thomas.herzog@gepardec.com>
2022-07-29 11:16:16 +0200

    Finished migration of drawings and added exersices to this repo as github pages

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@
 
 readonly IMAGE_NAME=containerization-training-renderer
 readonly PRESENTATION_NAME=Containerization-Training
-
+readonly OUTPUT_DIR=generated
 #################### 
 # GLOBAL VARIABLES #
 ####################
@@ -16,20 +16,20 @@ readonly PRESENTATION_NAME=Containerization-Training
 ##########
 set -ex
 
-rm -rf docs
-mkdir docs
+rm -rf ${OUTPUT_DIR}
+mkdir ${OUTPUT_DIR}
 docker build -t $IMAGE_NAME .
 docker container rm -f $IMAGE_NAME >/dev/null
 docker run -d --name $IMAGE_NAME $IMAGE_NAME
-docker cp $IMAGE_NAME:/opt/revealjs/dist   docs/dist
-docker cp $IMAGE_NAME:/opt/revealjs/plugin docs/plugin
-docker cp $IMAGE_NAME:/opt/revealjs/src    docs/src
+docker cp $IMAGE_NAME:/opt/revealjs/dist   ${OUTPUT_DIR}/dist
+docker cp $IMAGE_NAME:/opt/revealjs/plugin ${OUTPUT_DIR}/plugin
+docker cp $IMAGE_NAME:/opt/revealjs/src    ${OUTPUT_DIR}/src
 
-chmod -R 777 docs
+chmod -R 777 ${OUTPUT_DIR}
 
 docker container rm -f $IMAGE_NAME >/dev/null
 docker run -d -p 8080:8080 --entrypoint=/opt/revealjs/src/bin/present.py --name $IMAGE_NAME $IMAGE_NAME 8080 
 sleep 2
-docker cp $IMAGE_NAME:/opt/revealjs/index.html docs/$PRESENTATION_NAME.html
-#docker run --rm --net=host -t -v $(pwd):/slides astefanutti/decktape:3.1.0 reveal --size=1920x1080 --pause 2000 --load-pause 2000 http://localhost:8080/ docs/$PRESENTATION_NAME.pdf
+docker cp $IMAGE_NAME:/opt/revealjs/index.html ${OUTPUT_DIR}/$PRESENTATION_NAME.html
+#docker run --rm --net=host -t -v $(pwd):/slides astefanutti/decktape:3.1.0 reveal --size=1920x1080 --pause 2000 --load-pause 2000 http://localhost:8080/ ${OUTPUT_DIR}/$PRESENTATION_NAME.pdf
 docker container rm -f $IMAGE_NAME >/dev/null
