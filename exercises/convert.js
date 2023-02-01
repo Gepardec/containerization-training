eb44b58f9755ffd79c8fd1351f78e8c6db261c22
Thomas Herzog <thomas.herzog@gepardec.com>
2023-02-01 15:05:43 +0100

    Added highlightjs to assets. Custom css for exercises added. Finalized migration (copy-paste). Added meta-data for document. Refactored source,shell -> source,bash

--- exercises/convert.js
+++ exercises/convert.js
@@ -1,6 +1,16 @@
+// Arguments
+const revision = process.argv.revision || 'local'
+const revdate = new Date().toLocaleDateString("en-US")
 // Load Asciidoctor.js and the reveal.js converter
 var asciidoctor = require('@asciidoctor/core')()
 
 // Convert the document 'presentation.adoc' using the reveal.js converter
-var options = { safe: 'safe' }
+var options = { 
+    safe: 'safe',
+    attributes: {
+        linkcss: true,
+        revnumber: revision,
+        revdate: revdate
+    }
+}
 asciidoctor.convertFile('exercises.adoc', options) 
